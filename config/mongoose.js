2880e23be9d28312c1e1100dfb44b3e6db3a587e
Nitish Kumar Yadav <nitishyadav169@gmail.com>
2020-11-30 13:07:45 +0530

    create as simple habit tracker

--- config/mongoose.js
+++ config/mongoose.js
@@ -10,6 +10,7 @@ const db = mongoose.connection;
 // error
 db.on('error',console.error.bind(console,'error in connecting the database'));
 
+
 // up and running then print the message
 db.once('open',function(){
  console.log('Successfully connected to the database');
